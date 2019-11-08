b274b90c416a6741d27ec6d6067f34deab5a4802
Paulo Dutra <paulodutra.info@gmail.com>
2019-11-08 18:39:05 -0300

    criação do componente de sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Sidebar(props) {
+
+    return(
+        <aside id="sidebar">
+            <div className="sidebar-title">
+                <h2>Admin</h2>
+            </div>
+            <div className="list-group">
+                <a href="" className="list-group-item">Link</a>
+            </div>
+        </aside>
+    );
+
+}
--- src/containers/AppLayout.jsx
+++ src/containers/AppLayout.jsx
@@ -2,23 +2,26 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Templates from './routes';
 import Header from './../components/Header';
+import Sidebar from './../components/Sidebar';
 
 export default class AppLayout extends Component {
     render(){
         return (
 
-            <div id="main-wrapper">
-                {/*<Sidebar />*/}
-                <div class="content-wrapper">
+            <div className="d-flex" id="main-wrapper">
+                <Sidebar />
+                <div id="content-wrapper">
                     <Header />
                     <Switch>
                     { Templates.map((route, index) => (
-
-                        <Route 
-                            path={route.path}
-                            exact={route.exact}
-                            render={props => <route.template {...props} />} 
-                        />
+                        <div className="content-dynamic">
+                            <Route 
+                                key={index}
+                                path={route.path}
+                                exact={route.exact}
+                                render={props => <route.template {...props} />} 
+                            />
+                        </div>
                     ))}
                     </Switch>
                 </div>
